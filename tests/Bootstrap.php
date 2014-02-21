fdcc95e194d10144ee15356a5220fc5f60512a5a
Michel Maas <michel@michelmaas.com>
2014-02-21 20:14:22 +0100

    Modified Bootstrap.php

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,35 +1,109 @@
 <?php
-/*
- * Set error reporting to the level to which Zend Framework code must comply.
- */
-error_reporting( E_ALL | E_STRICT );
-
-/*
- * Determine the root, library, and tests directories of the framework
- * distribution.
- */
-$root        = realpath(dirname(__DIR__));
-$moduleSrc = "$root/src";
-$moduleTests   = "$root/tests";
-
-/*
- * Prepend the Zend Framework library/ and tests/ directories to the
- * include_path. This allows the tests to run out of the box and helps prevent
- * loading other copies of the framework code and tests that would supersede
- * this copy.
- */
-$path = array(
-    $moduleSrc,
-    $moduleTests,
-    get_include_path(),
-);
-set_include_path(implode(PATH_SEPARATOR, $path));
-
 /**
- * Setup autoloading
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
  */
-include __DIR__ . '/_autoload.php';
-/*
- * Unset global variables that are no longer needed.
- */
-unset($root, $moduleSrc, $moduleTests, $path);
+
+namespace AxalianAchievementsTest;
+
+use Zend\Loader\AutoloaderFactory;
+use RuntimeException;
+
+error_reporting(E_ALL | E_STRICT);
+chdir(__DIR__);
+
+class Bootstrap
+{
+    /**
+     * @var \Zend\ServiceManager\ServiceManager
+     */
+    protected static $serviceManager;
+
+    /**
+     * Init
+     */
+    public static function init()
+    {
+        static::initAutoloader();
+    }
+
+    /**
+     * Initialize the autoloader
+     *
+     * @throws \RuntimeException
+     */
+    protected static function initAutoloader()
+    {
+        $vendorPath = static::findParentPath('vendor');
+
+        if (is_readable($vendorPath . '/autoload.php')) {
+            $loader = include $vendorPath . '/autoload.php';
+
+            return;
+        }
+
+        $zf2Path = getenv('ZF2_PATH') ?: (defined('ZF2_PATH') ? ZF2_PATH : (is_dir($vendorPath . '/ZF2/library') ? $vendorPath . '/ZF2/library' : false));
+
+        if (!$zf2Path) {
+            throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
+        }
+
+        if (isset($loader)) {
+            $loader->add('Zend', $zf2Path . '/Zend');
+        } else {
+            include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
+            include $zf2Path . '/Zend/Loader/ClassMapAutoloader.php';
+            AutoloaderFactory::factory(
+                array(
+                    'Zend\Loader\ClassMapAutoloader' => array(array(
+                        'AxalianAchievements\Module' => __DIR__ . '/../Module.php',
+                    )),
+                    'Zend\Loader\StandardAutoloader' => array(
+                        'autoregister_zf' => true,
+                        'namespaces' => array(
+                            'AxalianAchievements' => __DIR__ . '/../src/AxalianAchievements',
+                            __NAMESPACE__ => __DIR__ . '/' . __NAMESPACE__,
+                        ),
+                    ),
+                )
+            );
+        }
+    }
+
+    /**
+     * Find the parent path
+     *
+     * @param $path
+     * @return bool|string
+     */
+    protected static function findParentPath($path)
+    {
+        $dir         = __DIR__;
+        $previousDir = '.';
+        while (!is_dir($dir . '/' . $path)) {
+            $dir = dirname($dir);
+
+            if ($previousDir === $dir) {
+                return false;
+            }
+
+            $previousDir = $dir;
+        }
+
+        return $dir . '/' . $path;
+    }
+}
+
+Bootstrap::init();
