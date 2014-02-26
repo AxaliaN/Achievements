f1e7f6c58ac959dbca9f489cd3d9e25647a1ed65
Michel Maas <michel@michelmaas.com>
2014-02-26 11:26:58 +0100

    Update AchievementAwarded.php

--- src/AxalianAchievements/View/Helper/AchievementAwarded.php
+++ src/AxalianAchievements/View/Helper/AchievementAwarded.php
@@ -5,7 +5,7 @@
  * @category AxalianAchievements\View\Helper
  * @package  AxalianAchievements\View\Helper
  * @author   Michel Maas <michel@michelmaas.com>
- * @method   \Zend\View\Helper\Partial partial($name = null, $values = null)
+ * @method   string \Zend\View\Helper\Partial partial($name = null, $values = null)
  */
 
 namespace AxalianAchievements\View\Helper;
